5640ed42b8e0dd53a764c410ef2bf046a04ac8ae
Otomaru3 <38760547+Otomaru3@users.noreply.github.com>
2020-03-30 09:46:33 +0900

    Create _make_ALLttl.bat

---
+++ _make_ALLttl.bat
@@ -0,0 +1,24 @@
+@echo off
+
+cd /d %~dp0
+
+rem ### 出力するファイル名を設定する
+set OutputFile=★ALLTTL.bat
+
+rem ### 無視する.ttlファイルを設定する
+set IgnoreFile=00_CreateFolder.ttl
+
+rem ### 各.ttlファイルを実行するまでの遅延時間の設定するプロンプト
+set /p INPUTSTR="Set delay time in second： "
+
+echo rem Made with %0 > %OutputFile%
+echo pause > %OutputFile%
+
+for %%f in (*.ttl) do (
+    if not %%f==%IgnoreFile% (
+        echo start %%f >> %OutputFile%
+        echo timeout /t %INPUTSTR% >> %OutputFile%
+    )
+)
+
+ 
